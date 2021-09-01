6dfe0b1ddc95877ab87696b1ffc8d551b2caaa51
fakynno <fakynno@gmail.com>
2021-09-01 06:23:52 -0300

    Adding class attributes

--- src/main/java/one/digitalinnovation/personapi/model/Person.java
+++ src/main/java/one/digitalinnovation/personapi/model/Person.java
@@ -1,5 +1,36 @@
 package one.digitalinnovation.personapi.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String cpf;
+
+    private LocalDate birthDate;
+
+    @Column(nullable = false)
+    private List<Phone> phoneList;
 }
